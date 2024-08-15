f796afc1fd98cf13a7534c069fb21166cb47e5e0
Alireza <alirezazamanidev80@gmail.com>
2024-08-15 14:50:38 +0330

    feat: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmDbConfig } from './configs/typeOrm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass:TypeOrmDbConfig,
+      inject:[TypeOrmDbConfig]
+    }),
+
+  ],
+  providers:[TypeOrmDbConfig]
 })
 export class AppModule {}
